250447114e5ce9f01f63aca690724a02a69fe7a3
Rafael-Barbosa <rafael_master00@hotmail.com>
2021-05-12 07:23:42 -0300

    V.02

--- Utilidades/kpoints-vasp.c
+++ Utilidades/kpoints-vasp.c
@@ -8,19 +8,19 @@ int main (int argc, char **argv)
 	int i;
 
 if(argc != 8){
-printf("digite:\n\n  %s x_i x_f n\n\nonde x_0, y_0 e z_0  são os  pontos iniciais, x_f é o ponto final e n é o número de pontos\n", argv[0]);
+printf("digite:\n\n  %s 0.0 0.0 0.0 0.5 0.5 0.5 100 \n\nonde x_0, y_0 e z_0  são os  pontos iniciais e x_1, y_1, z_1 são os pontos finais e depois o número de pontos\n", argv[0]);
 exit(0);
 }
 
 x0= atof(argv[1]);
 y0= atof(argv[2]);
-z0= atoi(argv[3]);
+z0= atof(argv[3]);
 
 x1= atof(argv[4]);
 y1= atof(argv[5]);
-z1= atoi(argv[6]);
+z1= atof(argv[6]);
 
-nkpoints= atoi(argv[7]);
+nkpoints= atof(argv[7]);
 
 //	printf("Enter starting point :\n");
 //	scanf("%f %f %f",&x0,&y0,&z0);
